ec2af86462dc964fc99f2f348873788c0dd09787
Etienne Sibeaux <esibeaux.pro@outlook.fr>
2022-04-07 00:03:19 +0200

    debut editor

---
+++ RuneFactoryNoMoreFrontiers/Assets/Editor/PlayerManager_Editor.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PlayerManager))]
+public class PlayerManager_Editor : Editor
+{
+    //private SerializedProperty sp_maxHp;
+}
